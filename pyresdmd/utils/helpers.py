82b01ca42aaee7d4697390a4cda897fb6bbdb640
George Coote <108678340+GeorgeCoote@users.noreply.github.com>
2026-04-18 01:19:43 +0100

    Implement force_h and force_h_vectorized
    Added helper functions to Hermitian-ize matrices

--- pyresdmd/utils/helpers.py
+++ pyresdmd/utils/helpers.py
@@ -29,3 +29,58 @@ def dist2(x : torch.Tensor, centres : torch.Tensor) -> torch.Tensor:
     centres = centres.unsqueeze(0) # change shape to (1, m, d)
     diff = x - centres # broadcasts to shape (n, m, d), where diff[i, j, k] = x[i, k] - centres[j, k]
     return torch.linalg.vector_norm(diff, ord = 2, dim = 2) # this will sum out the last dimension
+
+def force_h(A : torch.Tensor) -> torch.Tensor:
+    '''
+    Helper function to eliminate spurious imaginary parts (arising due to floating-point/other numerical error) in a matrix that is theoretically required to be Hermitian.
+
+    This is done by taking 1/2 (A + A^\ast)/2. This is always Hermitian. 
+
+    Further, if A is symmetric, the thus produced matrix retains the same real parts with spurious imaginary parts eliminated.
+    
+    Failing to do this can result in Cholesky failing. 
+    
+    Parameters 
+    ----------------------------------
+    A : torch.Tensor
+        Matrix to Hermitian-ize represented as tensor. 
+    
+    Returns 
+    ----------------------------------
+    torch.Tensor 
+        Hermitian-ized vector represented as tensor. 
+    
+    Raises 
+    ----------------------------------
+    None
+    '''
+    if A.shape[0] != A.shape[1]:
+        raise ValueError("A should be a square matrix.") 
+    return 0.5 * (A + A.conj().T)
+
+def force_h_vectorized(A : torch.Tensor) -> torch.Tensor:
+    '''
+    A vectorized version of force_h. Input should be a tensor of matrices (that is, a 3D tensor), each of which need to be Hermitianized.
+    
+    This is significantly faster than looping over all such matrices in Python and Hermitianizing them individually. 
+    
+    Parameters 
+    ----------------------------------
+    A : torch.Tensor
+        3D tensor of matrices to Hermitianize in the sense introduced in force_h. 
+    
+    Returns 
+    ----------------------------------
+    torch.Tensor 
+        3D tensor of Hermitianized matrices. 
+    
+    Raises 
+    ----------------------------------
+    ValueError 
+        If the size of the second and third dimension of the tensor are unequal, then the input tensor does not consist of 
+        square matrices, and hence trying to Hermitianize them will either result in unexpected behaviour or dimension 
+        errors.
+    '''
+    if A.shape[1] != A.shape[2]:
+        raise ValueError("Tensor A should consist of square matrices")
+    return 0.5 * (A + A.conj().transpose(-1, -2)) # transpose last two dimensions
